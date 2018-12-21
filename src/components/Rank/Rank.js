1ea35b068415599001d3597c1eb97c37544bd632
aussftw <shagronaus@gmail.com>
2018-12-21 21:52:39 +0200

    frontend stage finished

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Rank = () => {
+  return (
+    <div className='white f3'>
+        {'Aexander, your current rank is '} {'#3'}
+    </div>
+  );
+}
+
+export default Rank; 
